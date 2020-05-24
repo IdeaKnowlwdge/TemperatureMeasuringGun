c0a4425c037d5a2245a6f987eb0db374cb9ab146
dongfangqing <641251565@qq.com>
2020-05-24 11:35:05 +0800

    modify MLX90614 drivers.

--- User/Src/MLX90614_Driver.C
+++ User/Src/MLX90614_Driver.C
@@ -1,7 +1,8 @@
 #include "MLX90614_Driver.h"
 #include "SysTick_Driver.h"
 #include "stdint.h"
-
+#include "i2c_driver.h"
+#include "include.h"
 
 
 #define ACK	 0 //应答
@@ -11,290 +12,6 @@
 #define EEPROM_ACCESS	0x20 //EEPROM access command EEPROM存取命令
 #define RAM_TOBJ1		0x07 //To1 address in the eeprom 目标1温度,检测到的红外温度 -70.01 ~ 382.19度
 
-#define SMBUS_PORT	GPIOB      //PB端口(端口和下面的两个针脚可自定义)
-#define SMBUS_SCK		GPIO_PIN_10 //PB10：SCL
-#define SMBUS_SDA		GPIO_PIN_11 //PB11：SDA
-
-#define MLX90614_GPIO_CLK_ENABLE()   __HAL_RCC_GPIOB_CLK_ENABLE()
-
-//SCL 引脚宏定义
-#define SMBUS_SCK_H()	    HAL_GPIO_WritePin(SMBUS_PORT, SMBUS_SCK,GPIO_PIN_SET)//置高电平
-#define SMBUS_SCK_L()	    HAL_GPIO_WritePin(SMBUS_PORT, SMBUS_SCK,GPIO_PIN_RESET)//置低电平
-//SDA 引脚宏定义
-#define SMBUS_SDA_H()	    HAL_GPIO_WritePin(SMBUS_PORT, SMBUS_SDA,GPIO_PIN_SET) 
-#define SMBUS_SDA_L()	    HAL_GPIO_WritePin(SMBUS_PORT, SMBUS_SDA,GPIO_PIN_RESET)
-
-//#define SMBUS_SDA_PIN()	    SMBUS_PORT->IDR & SMBUS_SDA //读取引脚电平
-#define SMBUS_SDA_PIN() HAL_GPIO_ReadPin(SMBUS_PORT, SMBUS_SDA)
-
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
-
-/*******************************************************************************
-* Function Name  : SMBus_StartBit
-* Description    : Generate START condition on SMBus
-* Input          : None
-* Output         : None
-* Return         : None
-*******************************************************************************/
-void SMBus_StartBit(void)
-{
-    SMBUS_SDA_H();		// Set SDA line
-    SMBus_Delay(1);	    // Wait a few microseconds
-    SMBUS_SCK_H();		// Set SCL line
-    SMBus_Delay(5);	    // Generate bus free time between Stop
-    SMBUS_SDA_L();		// Clear SDA line
-    SMBus_Delay(10);	    // Hold time after (Repeated) Start
-    // Condition. After this period, the first clock is generated.
-    //(Thd:sta=4.0us min)在SCK=1时，检测到SDA由1到0表示通信开始（下降沿）
-    SMBUS_SCK_L();	    // Clear SCL line
-    SMBus_Delay(2);	    // Wait a few microseconds
-}
-
-/*******************************************************************************
-* Function Name  : SMBus_StopBit
-* Description    : Generate STOP condition on SMBus
-* Input          : None
-* Output         : None
-* Return         : None
-*******************************************************************************/
-void SMBus_StopBit(void)
-{
-    SMBUS_SCK_L();		// Clear SCL line
-    SMBus_Delay(5);	    // Wait a few microseconds
-    SMBUS_SDA_L();		// Clear SDA line
-    SMBus_Delay(5);	    // Wait a few microseconds
-    SMBUS_SCK_H();		// Set SCL line
-    SMBus_Delay(10);	    // Stop condition setup time(Tsu:sto=4.0us min)
-    SMBUS_SDA_H();		// Set SDA line在SCK=1时，检测到SDA由0到1表示通信结束（上升沿）
-}
-
-/*******************************************************************************
-* Function Name  : SMBus_SendByte
-* Description    : Send a byte on SMBus
-* Input          : Tx_buffer
-* Output         : None
-* Return         : None
-*******************************************************************************/
-uint8_t SMBus_SendByte(uint16_t Tx_buffer)
-{
-    uint8_t	Bit_counter;
-    uint8_t	Ack_bit;
-    uint8_t	bit_out;
-
-    for(Bit_counter=8; Bit_counter; Bit_counter--)
-    {
-        if (Tx_buffer&0x80)
-        {
-            bit_out=1;   // If the current bit of Tx_buffer is 1 set bit_out
-        }
-        else
-        {
-            bit_out=0;  // else clear bit_out
-        }
-        SMBus_SendBit(bit_out);		// Send the current bit on SDA
-        Tx_buffer<<=1;				// Get next bit for checking
-    }
-
-    Ack_bit=SMBus_ReceiveBit();		// Get acknowledgment bit
-    return	Ack_bit;
-}
-
-/*******************************************************************************
-* Function Name  : SMBus_SendBit
-* Description    : Send a bit on SMBus 82.5kHz
-* Input          : bit_out
-* Output         : None
-* Return         : None
-*******************************************************************************/
-void SMBus_SendBit(uint8_t bit_out)
-{
-    if(bit_out==0)
-    {
-        SMBUS_SDA_L();
-    }
-    else
-    {
-        SMBUS_SDA_H();
-    }
-    SMBus_Delay(2);					// Tsu:dat = 250ns minimum
-    SMBUS_SCK_H();					// Set SCL line
-    SMBus_Delay(10);					// High Level of Clock Pulse
-    SMBUS_SCK_L();					// Clear SCL line
-    SMBus_Delay(10);					// Low Level of Clock Pulse
-//	SMBUS_SDA_H();				    // Master release SDA line ,
-    return;
-}
-
-/*******************************************************************************
-* Function Name  : SMBus_ReceiveBit
-* Description    : Receive a bit on SMBus
-* Input          : None
-* Output         : None
-* Return         : Ack_bit
-*******************************************************************************/
-uint8_t SMBus_ReceiveBit(void)
-{
-    uint8_t Ack_bit;
-
-	
-    SMBUS_SDA_H();          //引脚靠外部电阻上拉，当作输入
-//  	SMBus_Delay(2);			// High Level of Clock Pulse
-    SMBUS_SCK_H();			// Set SCL line
-    SMBus_Delay(2);			// High Level of Clock Pulse
-    if (SMBUS_SDA_PIN())
-    {
-        Ack_bit=1;
-    }
-    else
-    {
-        Ack_bit=0;
-    }
-    SMBUS_SCK_L();			// Clear SCL line
-    SMBus_Delay(4);			// Low Level of Clock Pulse
-
-    return	Ack_bit;
-}
-
-/*******************************************************************************
-* Function Name  : SMBus_ReceiveByte
-* Description    : Receive a byte on SMBus
-* Input          : ack_nack
-* Output         : None
-* Return         : RX_buffer
-*******************************************************************************/
-uint8_t SMBus_ReceiveByte(uint8_t ack_nack)
-{
-    uint8_t 	RX_buffer;
-    uint8_t	Bit_Counter;
-
-    for(Bit_Counter=8; Bit_Counter; Bit_Counter--)
-    {
-        if(SMBus_ReceiveBit())			// Get a bit from the SDA line
-        {
-            RX_buffer <<= 1;			// If the bit is HIGH save 1  in RX_buffer
-            RX_buffer |=0x01;
-        }
-        else
-        {
-            RX_buffer <<= 1;			// If the bit is LOW save 0 in RX_buffer
-            RX_buffer &=0xfe;
-        }
-    }
-    SMBus_SendBit(ack_nack);			// Sends acknowledgment bit
-    return RX_buffer;
-}
-
-/*******************************************************************************
-* Function Name  : SMBus_Delay
-* Description    : 延时  一次循环约1us
-* Input          : time
-* Output         : None
-* Return         : None
-*******************************************************************************/
-void SMBus_Delay(uint16_t time)
-{
-    uint16_t i, j;
-    for (i=0; i<4; i++)
-    {
-        for (j=0; j<time; j++);
-    }
-}
-
-/*******************************************************************************
-* Function Name  : SMBus_Init
-* Description    : SMBus初始化
-* Input          : None
-* Output         : None
-* Return         : None
-*******************************************************************************/
-void SMBus_Init()
-{
-   	/* Enable SMBUS_PORT clocks */
-	GPIO_InitTypeDef GPIO_InitStruct; //声明一个结构体变量
-   
-	MLX90614_GPIO_CLK_ENABLE();//非接触温度传感器SDAL 连接PB.15,SCL连接PB.14---打开GPIOD时钟
-	
-	// 温度传感器引脚配置
-	GPIO_InitStruct.Pin = (SMBUS_SCK|SMBUS_SDA);          //非接触温度传感器SDAL 
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;        //管脚频率为50MHZ
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;          //输出模式为
-  GPIO_InitStruct.Pull  = GPIO_PULLUP;    					   //设置引脚为上拉模式
-	
-	HAL_GPIO_Init(GPIOB,&GPIO_InitStruct);                    //初始化PB 10/11
-	
-    SMBUS_SCK_H();
-    SMBUS_SDA_H();	  
-
-}
-
-
-
-
-/*******************************************************************************
- * Function Name  : SMBus_ReadMemory
- * Description    : READ DATA FROM RAM/EEPROM
- * Input          : slaveAddress, command
- * Output         : None
- * Return         : Data
-*******************************************************************************/
-uint16_t SMBus_ReadMemory(uint8_t slaveAddress, uint8_t command)
-{
-    uint16_t data;			// Data storage (DataH:DataL)
-    uint8_t Pec;				// PEC byte storage
-    uint8_t DataL=0;			// Low data byte storage
-    uint8_t DataH=0;			// High data byte storage
-    uint8_t arr[6];			// Buffer for the sent bytes
-    uint8_t PecReg;			// Calculated PEC byte storage
-    uint8_t ErrorCounter;	// Defines the number of the attempts for communication with MLX90614
-
-    ErrorCounter=0x00;				// Initialising of ErrorCounter
-	slaveAddress <<= 1;	//2-7位表示从机地址
-	
-    do
-    {
-repeat:
-        SMBus_StopBit();			    //If slave send NACK stop comunication
-        --ErrorCounter;				    //Pre-decrement ErrorCounter
-        if(!ErrorCounter) 			    //ErrorCounter=0?
-        {
-            break;					    //Yes,go out from do-while{}
-        }
-
-        SMBus_StartBit();				//Start condition
-        if(SMBus_SendByte(slaveAddress))//Send SlaveAddress 最低位Wr=0表示接下来写命令
-        {
-            goto	repeat;			    //Repeat comunication again
-        }
-        if(SMBus_SendByte(command))	    //Send command
-        {
-            goto	repeat;		    	//Repeat comunication again
-        }
-
-        SMBus_StartBit();					//Repeated Start condition
-        if(SMBus_SendByte(slaveAddress+1))	//Send SlaveAddress 最低位Rd=1表示接下来读数据
-        {
-            goto	repeat;             	//Repeat comunication again
-        }
-
-        DataL = SMBus_ReceiveByte(ACK);	//Read low data,master must send ACK
-        DataH = SMBus_ReceiveByte(ACK); //Read high data,master must send ACK
-        Pec = SMBus_ReceiveByte(NACK);	//Read PEC byte, master must send NACK
-        SMBus_StopBit();				//Stop condition
-
-        arr[5] = slaveAddress;		//
-        arr[4] = command;			//
-        arr[3] = slaveAddress+1;	//Load array arr
-        arr[2] = DataL;				//
-        arr[1] = DataH;				//
-        arr[0] = 0;					//
-        PecReg=PEC_Calculation(arr);//Calculate CRC
-    }
-    while(PecReg != Pec);		//If received and calculated CRC are equal go out from do-while{}
-
-	data = (DataH<<8) | DataL;	//data=DataH:DataL
-    return data;
-}
 
 /*******************************************************************************
 * Function Name  : PEC_calculation
@@ -378,34 +95,152 @@ uint8_t PEC_Calculation(uint8_t pec[])
     return pec[0];
 }
 
- /*******************************************************************************
- * Function Name  : SMBus_ReadTemp
- * Description    : Calculate and return the temperature
- * Input          : None
- * Output         : None
- * Return         : SMBus_ReadMemory(0x00, 0x07)*0.02-273.15
-*******************************************************************************/
-float SMBus_ReadTemp(void)
-{   
-	float temp;
-	temp = SMBus_ReadMemory(SA, RAM_ACCESS|RAM_TOBJ1)*0.02-273.15;
-	//temp = SMBus_ReadMemory(0x00, 0x07)*0.02-273.15;
-	return temp;
+
+i2c_device INFRARED_dev = {
+	.sof_i2c = &sof_i2c2,
+	.slave_addr = SA,
+};
+
+/******************************************************************
+*	函 数 名: i2c_dev_ReadBytes
+*	功能说明: 从串行EEPROM指定地址处开始读取若干数据
+*	形    参：dev-------:I2C总线上的设备
+*			 _usAddress : 起始地址
+*			 _usSize : 数据长度，单位为字节
+*			 _pReadBuf : 存放读到的数据的缓冲区指针
+*	返 回 值: 0 表示失败，1表示成功
+********************************************************************/
+uint8_t i2c_dev_ReadBytes(i2c_device* dev,
+							uint8_t *_pReadBuf, uint16_t _usAddress, uint16_t _usSize)
+{
+	uint16_t i;
+	
+	I2C_Start(dev->sof_i2c);
+	I2C_SendByte(dev->sof_i2c,dev->slave_addr|I2C_WR);
+	
+	/* 第3步：发送ACK */
+	if (I2C_RecvACK(dev->sof_i2c) != 0)
+	{
+		goto cmd_fail;	
+	}
+
+	/* 第4步：发送字节地址 */
+	I2C_SendByte(dev->sof_i2c,(uint8_t)_usAddress&0xff);
+	
+	/* 第5步：发送ACK */
+	if (I2C_RecvACK(dev->sof_i2c) != 0)
+	{
+		goto cmd_fail;	/* 器件无应答 */
+	}
+	
+	/* 第6步：重新启动I2C总线。下面开始读取数据 */
+	I2C_Start(dev->sof_i2c);
+	
+	/* 第7步：发起控制字节，高7bit是地址，bit0是读写控制位，0表示写，1表示读 */
+	I2C_SendByte(dev->sof_i2c,dev->slave_addr|I2C_RD);
+	/* 第8步：发送ACK */
+	if (I2C_RecvACK(dev->sof_i2c) != 0)
+	{
+		goto cmd_fail;	/* 器件无应答 */
+	}	
+	
+	/* 第9步：循环读取数据 */
+	for (i = 0; i < _usSize; i++)
+	{
+		_pReadBuf[i] = I2C_RecvByte(dev->sof_i2c);	/* 读1个字节 */
+		
+		/* 每读完1个字节后，需要发送Ack， 最后一个字节不需要Ack，发Nack */
+		if (i != _usSize - 1)
+		{
+			I2C_SendACK(dev->sof_i2c,0);	/* 中间字节读完后，CPU产生ACK信号(驱动SDA = 0) */
+		}
+		else
+		{
+			I2C_SendACK(dev->sof_i2c,1);	/* 最后1个字节读完后，CPU产生NACK信号(驱动SDA = 1) */
+		}
+		delay_us(100);
+	}
+	
+	/* 发送I2C总线停止信号 */
+	I2C_Stop(dev->sof_i2c);
+	return 0;	/* 执行成功 */
+
+cmd_fail: /* 命令执行失败后，切记发送停止信号，避免影响I2C总线上其他设备 */
+	/* 发送I2C总线停止信号 */
+	I2C_Stop(dev->sof_i2c);
+//	printf("Read fild!\r\n");
+	return 1;
 }
 
-/*********************************END OF FILE*********************************/
 
 
+int infrared_read_data(i2c_device* dev,uint8_t checksum_array[],uint8_t* crc_checksum)
+{
+	int ret = 0;
+	uint8_t ReadBuf[3] = {0};
 
+	ret = i2c_dev_ReadBytes(dev,ReadBuf, RAM_ACCESS|RAM_TOBJ1,3);
+	if(ret)
+	{
+		return -1;
+	}
 
+	checksum_array[5] = dev->slave_addr;		//device address and write bit
+	checksum_array[4] = RAM_ACCESS|RAM_TOBJ1;	//
+	checksum_array[3] = dev->slave_addr|1;	//device address and read bit
+	checksum_array[2] = ReadBuf[0];			//Low data bit
+	checksum_array[1] = ReadBuf[1];				//high data bit
+	checksum_array[0] = 0;					//
+	
+	*crc_checksum = ReadBuf[2];
+	
+	return 0;
+}
 
+int get_infrared_data(uint16_t* data)
+{
+	int ret = 0;
+	i2c_device* dev = &INFRARED_dev;
+	uint8_t checksum_array[6] = {0};
+	uint8_t crc_checksum = 0;
+	uint8_t PecReg;				// PEC byte storage
+    uint8_t DataL=0;			// Low data byte storage
+    uint8_t DataH=0;			// High data byte storage
 
+	ret = infrared_read_data(dev,checksum_array,&crc_checksum);
+	if(ret)
+	{
+		return ret;
+	}
 
+	DataL = checksum_array[2];
+	DataH = checksum_array[1];
+	PecReg=PEC_Calculation(checksum_array);//Calculate CRC
 
+	if(crc_checksum != PecReg)
+	{
+		return -1;
+	}
 
+	*data = (DataH<<8) | DataL;	//data=DataH:DataL
+	
+	return ret;
+}
 
+float infrared_ReadTemp(void)
+{
+	int status = 0;
+	uint16_t data = 0;
+	float temp;
 
+	status = get_infrared_data(&data);
+	if(status)
+	{
+		return 0;
+	}
 
+	temp = (float)data * 0.02 - 273.15;;
 
-
+	return temp;
+}
 
--- User/inc/MLX90614_Driver.h
+++ User/inc/MLX90614_Driver.h
@@ -16,20 +16,7 @@
 //#define RAM_TOBJ1     0x07 //To1 address in the eeprom
 
 
-
-void SMBus_StartBit(void);
-void SMBus_StopBit(void);
-void SMBus_SendBit(uint8_t);
-uint8_t SMBus_SendByte(uint16_t);
-uint8_t SMBus_ReceiveBit(void);
-uint8_t SMBus_ReceiveByte(uint8_t);
-void SMBus_Delay(uint16_t);
-void SMBus_Init(void);
-
-uint16_t SMBus_ReadMemory(uint8_t, uint8_t);
-uint8_t PEC_Calculation(uint8_t*);
-float SMBus_ReadTemp(void); //获取温度值
-
+float infrared_ReadTemp(void);
 
 #endif
 
