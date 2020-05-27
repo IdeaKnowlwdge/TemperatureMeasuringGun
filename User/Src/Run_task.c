fe594f586cd2239aeb4a7212cd547db0a25ead5b
dongfangqing <641251565@qq.com>
2020-05-27 23:08:21 +0800

    add key function case.

--- User/Src/Run_task.c
+++ User/Src/Run_task.c
@@ -16,24 +16,60 @@ static void Run_Task(void)
 		switch(msg)
 		{
 			case MSG_IOKEY1_SHORT:
-				OWL_BOARD_DEBUG("MSG_IOKEY_SHORT\r\n");
+				OWL_BOARD_DEBUG("MSG_IOKEY1_SHORT\r\n");
 				break;
 			case MSG_IOKEY2_SHORT:
-				OWL_BOARD_DEBUG("MSG_ADKEY_1_SHORT\r\n");
+				OWL_BOARD_DEBUG("MSG_IOKEY2_SHORT\r\n");
 				break;
 			case MSG_IOKEY3_SHORT:
-				OWL_BOARD_DEBUG("MSG_ADKEY_2_SHORT\r\n");
+				OWL_BOARD_DEBUG("MSG_IOKEY3_SHORT\r\n");
 				break;
 			case MSG_IOKEY4_SHORT:
-				OWL_BOARD_DEBUG("MSG_ADKEY_3_SHORT\r\n");
+				OWL_BOARD_DEBUG("MSG_IOKEY4_SHORT\r\n");
+				break;
+			case MSG_IOKEY1_HOLD:
+				OWL_BOARD_DEBUG("MSG_IOKEY1_HOLD\r\n");
+				break;
+			case MSG_IOKEY2_HOLD:
+				OWL_BOARD_DEBUG("MSG_IOKEY2_HOLD\r\n");
+				break;
+			case MSG_IOKEY3_HOLD:
+				OWL_BOARD_DEBUG("MSG_IOKEY3_HOLD\r\n");
+				break;
+			case MSG_IOKEY4_HOLD:
+				OWL_BOARD_DEBUG("MSG_IOKEY4_HOLD\r\n");
+				break;
+			case MSG_IOKEY1_LONG:
+				OWL_BOARD_DEBUG("MSG_IOKEY1_LONG\r\n");
+				break;
+			case MSG_IOKEY2_LONG:
+				OWL_BOARD_DEBUG("MSG_IOKEY2_LONG\r\n");
+				break;
+			case MSG_IOKEY3_LONG:
+				OWL_BOARD_DEBUG("MSG_IOKEY3_LONG\r\n");
+				break;
+			case MSG_IOKEY4_LONG:
+				OWL_BOARD_DEBUG("MSG_IOKEY4_LONG\r\n");
+				break;
+			case MSG_IOKEY1_LONG_UP:
+				OWL_BOARD_DEBUG("MSG_IOKEY1_LONG_UP\r\n");
+				break;
+			case MSG_IOKEY2_LONG_UP:
+				OWL_BOARD_DEBUG("MSG_IOKEY2_LONG_UP\r\n");
+				break;
+			case MSG_IOKEY3_LONG_UP:
+				OWL_BOARD_DEBUG("MSG_IOKEY3_LONG_UP\r\n");
+				break;
+			case MSG_IOKEY4_LONG_UP:
+				OWL_BOARD_DEBUG("MSG_IOKEY4_LONG_UP\r\n");
 				break;
 			case MSG_100MS:
 				break;
 			case MSG_200MS:
-				OWL_BOARD_DEBUG("MSG_200MS\r\n");
+//				OWL_BOARD_DEBUG("MSG_200MS\r\n");
 				break;
 			case MSG_HALF_SECOND:
-				OWL_BOARD_DEBUG("MSG_HALF_SECOND\r\n");
+//				OWL_BOARD_DEBUG("MSG_HALF_SECOND\r\n");
 				break;
 			case MSG_800MS:
 				break;
